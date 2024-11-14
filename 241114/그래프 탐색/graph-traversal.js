a8e6af150f65fd058b1615ea78342b04a458e543
UTSU2 <125986716+UTSU2@users.noreply.github.com>
2024-11-14 13:12:19 +0900

    Title: 그래프 탐색, Time: 254ms, Memory: 18MB, Status: Passed - CodeTree

--- 241114/그래프 탐색/graph-traversal.js
+++ 241114/그래프 탐색/graph-traversal.js
@@ -1,32 +1,37 @@
-function countReachableNodes(N, M, edges) {
-    const graph = Array.from({ length: N + 1 }, () => []);
-    
-    edges.forEach(([u, v]) => {
-        graph[u].push(v);
-        graph[v].push(u);
-    });
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
 
-    const visited = Array(N + 1).fill(false);
+// 변수 선언 및 입력
+const [n, m] = input[0].split(' ').map(Number);
 
-    function dfs(node) {
-        visited[node] = true;
-        let count = 0;
+// index를 1번부터 사용하기 위해 m+1만큼 할당합니다.
+const graph = Array.from(Array(n + 1), () => []);
 
-        for (const next of graph[node]) {
-            if (!visited[next]) { 
-                count += 1 + dfs(next);
-            }
+let visited = Array(n + 1).fill(false);
+let vertexCnt = 0;
+
+function dfs(vertex) {
+    // 해당 정점에서 이어져있는 모든 정점을 탐색해줍니다.
+    graph[vertex].forEach(currV => {
+        // 아직 간선이 존재하고 방문한 적이 없는 정점에 대해서만 탐색을 진행합니다.
+        if (!visited[currV]) {
+            visited[currV] = true;
+            vertexCnt++;
+            dfs(currV);
         }
-        
-        return count;
-    }
-    return dfs(1);
+    });
+};
+
+for (let i = 1; i <= m; i++) {
+    const [v1, v2] = input[i].trim().split(' ').map(Number);
+
+    // 각 정점이 서로 이동이 가능한 양방향 그래프이기 때문에 
+    // 각 정점에 대한 간선을 각각 저장해줍니다.
+    graph[v1].push(v2);
+    graph[v2].push(v1);
 }
 
-const N = 3;
-const M = 2;
-const edges = [
-    [1, 2],
-    [1, 3]
-];
-console.log(countReachableNodes(N, M, edges));
+visited[1] = true;
+dfs(1);
+
+console.log(vertexCnt);
