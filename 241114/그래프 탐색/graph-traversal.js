56eac84dd30f4c100bb505996350d40526283368
UTSU2 <125986716+UTSU2@users.noreply.github.com>
2024-11-14 13:38:00 +0900

    Title: 그래프 탐색, Time: 203ms, Memory: 18MB, Status: Passed - CodeTree

--- 241114/그래프 탐색/graph-traversal.js
+++ 241114/그래프 탐색/graph-traversal.js
@@ -1,16 +1,11 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const [n,m] = input[0].split(' ').map(Number);
+const [n, m] = input[0].split(' ').map(Number);
 const graph = Array.from(Array(n+1), ()=>[]);
+
 let visited = Array(n+1).fill(false);
 let vertexCnt = 0;
-
-for (let i = 1; i<=m; i++){
-    const [v1, v2] = input[i].trim().split(' ').map(Number);
-    graph[v1].push[v2];
-    graph[v2].push[v1];
-}
 
 function dfs(vertex) {
     graph[vertex].forEach(currV =>{
@@ -20,6 +15,12 @@ function dfs(vertex) {
             dfs(currV);
         }
     });
+}
+
+for (let i = 1; i<=m; i++){
+    const [v1, v2] = input[i].trim().split(' ').map(Number);
+    graph[v1].push(v2);
+    graph[v2].push(v1);
 }
 
 visited[1] = true;
